cf21862117ef8b3ebfd711f6d269906905a128ea
allison ruiz <allisonruiz@allisons-MacBook-Pro.local>
2022-04-02 01:06:32 -0700

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(path, left, bottom){
+    let image = document.createElement('img')
+    image.src = path
+    image.style.position = 'fixed'
+    image.style.left = left
+    image.style.bottom = bottom
+    document.body.append(image)
+}
+function newItem(path, left, bottom) {
+    let item = document.createElement('img')
+    item.src = path
+    item.style.position = 'fixed'
+    item.style.left = left
+    item.style.bottom = bottom
+    document.body.append(item)
+item.addEventListener('dblclick', function() {
+    item.remove()
+})
+}
+function background(path, left, bottom) {
+    let screenWidth = window.innerWidth;
+    let screenHeight = window.innerHeight;
+    let horizonDistance = PositionBottom + 5;
+    for(let i= 0; i<screenWidth; i++)
+    newImage()
+    
+}
+
+
+newImage('assets/green-character.gif', '100px', '100px');
+newImage('assets/pine-tree.png', '450px', '200px');
+newImage('assets/tree.png', '200px', '300px');
+newImage('assets/pillar.png', '350px', '100px');
+newImage('assets/crate.png', '150px', '200px');
+newImage('assets/well.png', '500px', '425px');
+newItem('assets/sword.png', '500px', '450px');
+newItem('assets/sheild.png', '165px', '185px');
+newItem('assets/staff.png', '600px', '100px');
